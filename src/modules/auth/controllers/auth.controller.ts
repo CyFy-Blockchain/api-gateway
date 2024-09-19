0ab7fbbf73833df8e7eb36ad585541729c0e936f
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2024-09-19 08:27:17 +0500

    Swagger: minor improvements.

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SWAGGER_TAGS } from 'src/config/swagger/tags';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignUpDto } from '../dto/auth.dto';
 import { lastValueFrom } from 'rxjs';
 import { AuthClientService } from '../libs/grpc.auth.lib';
@@ -17,6 +17,9 @@ export class AuthController {
    * @returns A boolean value indicating the success of the signup operation.
    */
   @Post()
+  @ApiOperation({ summary: 'Signup user' })
+  @ApiResponse({ status: 201, description: 'Success.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async signup(@Body() signupInput: SignUpDto): Promise<boolean> {
     try {
       const signupResponse = await lastValueFrom(
--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty({ description: 'The name of the user.', example: 'John Doe' })
   @IsNotEmpty({ message: 'Username is required' })
   @IsString({ message: 'Username must be a string' })
   username: string;
 
+  @ApiProperty({
+    description: 'The password of the user.',
+    example: 'Abcd@1234',
+  })
   @IsNotEmpty({ message: 'Password is required' })
   @IsString({ message: 'Password must be a string' })
   password: string;
