72bfc63fcff2b32908e10fdc9d9bc7129a2a1716
M Hassan Tariq <mhtariq29@gmail.com>
2024-12-07 16:38:26 +0500

    docs: updated the swagger documention

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -5,7 +5,7 @@ export const swaggerConfig = new DocumentBuilder()
   .setDescription('This documentation is for cyfy blockchain API documentation')
   .setVersion('1.0.0')
   .addServer('http://localhost:3000', 'Local environment')
-  .addServer('http://192.168.20.53:3000', 'Dev enviroment')
+  .addServer('https://api-gateway-990q.onrender.com/', 'Dev enviroment')
   .addApiKey(
     { type: 'apiKey', name: 'x-api-key', in: 'header' },
     'api-gateway-key',
